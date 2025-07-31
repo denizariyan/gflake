e7f2e49bee581f67eb30c75332508d7403d70b39
Deniz Ariyan <denizariyan@gmail.com>
2025-07-31 19:01:07 +0200

    Add test binary

---
+++ tests/sample_tests.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+#include <chrono>
+#include <thread>
+
+class BasicTests : public ::testing::Test {
+protected:
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(BasicTests, FastTest) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST_F(BasicTests, SlowTest) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EXPECT_TRUE(true);
+}
+
+TEST_F(BasicTests, VerySlowTest) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    EXPECT_TRUE(true);
+}
+
+class MathTests : public ::testing::Test {};
+
+TEST_F(MathTests, Addition) {
+    EXPECT_EQ(5 + 3, 8);
+}
+
+TEST_F(MathTests, Multiplication) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    EXPECT_EQ(4 * 3, 12);
+}
+
+TEST_F(MathTests, Division) {
+    EXPECT_EQ(10 / 2, 5);
+}
+
+class ParameterizedTest : public ::testing::TestWithParam<int> {};
+
+TEST_P(ParameterizedTest, IsEven) {
+    int value = GetParam();
+    EXPECT_EQ(value % 2, 0);
+}
+
+INSTANTIATE_TEST_SUITE_P(EvenNumbers, ParameterizedTest, 
+                         ::testing::Values(2, 4, 6, 8, 10));
+
+template <typename T>
+class TypedTest : public ::testing::Test {};
+
+using MyTypes = ::testing::Types<int, float, double>;
+TYPED_TEST_SUITE(TypedTest, MyTypes);
+
+TYPED_TEST(TypedTest, DefaultConstruction) {
+    TypeParam value{};
+    EXPECT_EQ(value, TypeParam{});
+}
+
+TYPED_TEST(TypedTest, Assignment) {
+    TypeParam value{};
+    value = static_cast<TypeParam>(42);
+    EXPECT_EQ(value, static_cast<TypeParam>(42));
+}
+
+// Not part of a fixture
+TEST(SimpleTests, TrueIsTrue) {
+    EXPECT_TRUE(true);
+}
+
+TEST(SimpleTests, FalseIsFalse) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(25));
+    EXPECT_FALSE(false);
+}
